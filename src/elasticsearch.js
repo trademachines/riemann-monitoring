80fd8c3261ac03982f51213d5874e7ac6a186f72
anho <me@anho.github.com>
2017-01-25 20:47:48 +0100

    add some metrics for merge stats

--- src/elasticsearch.js
+++ src/elasticsearch.js
@@ -5,86 +5,122 @@ const http  = require('./_http-stuff');
 let last = {};
 
 module.exports         = (runner) => {
-    runner.run((opts, r, done) => {
-        const host    = `${opts[ 'es-host' ]}:${opts[ 'es-port' ]}`;
-        const baseUrl = `http://${host}/`;
+  runner.run((opts, r, done) => {
+    const host    = `${opts['es-host']}:${opts['es-port']}`;
+    const baseUrl = `http://${host}/`;
 
-        async.parallel([
-            (cb) => {
-                http.json(`${baseUrl}_cluster/health`, (err, json) => {
-                    if (err) return cb(err);
+    async.parallel([
+      (cb) => {
+        http.json(`${baseUrl}_cluster/health`, (err, json) => {
+          if (err) {
+            return cb(err);
+          }
 
-                    _.each(_.omit(json, 'timed_out', 'status', 'cluster_name'), (v, k) => {
-                        r({ service: `elasticsearch.cluster.${k}`, metric: v });
-                    });
+          _.each(_.omit(json, 'timed_out', 'status', 'cluster_name'), (v, k) => {
+            r({service: `elasticsearch.cluster.${k}`, metric: v});
+          });
 
-                    cb();
-                });
-            },
-            (cb) => async.each(opts[ 'es-search-index' ], (index, cb) => {
-                http.json(`${baseUrl}${index}/_stats`, (err, json) => {
-                    if (err) return cb(err);
+          cb();
+        });
+      },
+      (cb) => async.each(opts['es-search-index'], (index, cb) => {
+        http.json(`${baseUrl}${index}/_stats`, (err, json) => {
+          if (err) {
+            return cb(err);
+          }
 
-                    _.each([ 'docs', 'store' ], (t) => {
-                        _.each(_.get(json, `_all.primaries.${t}`, {}), (v, k) => {
-                            r({ service: `elasticsearch.${t}.${index}.${k}`, metric: v });
-                        });
-                    });
+          _.each(['docs', 'store'], (t) => {
+            _.each(_.get(json, `_all.primaries.${t}`, {}), (v, k) => {
+              r({service: `elasticsearch.${t}.${index}.${k}`, metric: v});
+            });
+          });
 
-                    const info = _.get(json, '_all.total.search', {
-                        query_time_in_millis: 0,
-                        query_total: 1,
-                        fetch_time_in_millis: 0,
-                        fetch_total: 1
-                    });
-                    info.query = info.query_time_in_millis / (info.query_total || 1);
-                    info.fetch = info.fetch_time_in_millis / (info.fetch_total || 1);
+          const searchInfo = _.get(json, '_all.total.search', {
+            query_time_in_millis: 0,
+            query_total: 1,
+            fetch_time_in_millis: 0,
+            fetch_total: 1
+          });
+          const mergeInfo  = _.get(json, '_all.total.merges', {
+            total: 1,
+            total_docs: 1,
+            total_size_in_bytes: 0,
+            total_time_in_millis: 0,
+          });
+          searchInfo.query = searchInfo.query_time_in_millis / (searchInfo.query_total || 1);
+          searchInfo.fetch = searchInfo.fetch_time_in_millis / (searchInfo.fetch_total || 1);
 
-                    if (last[ index ]) {
-                        const queryTotalDiff = info.query_total - last[ index ].query_total;
-                        const fetchTotalDiff = info.fetch_total - last[ index ].fetch_total;
-                        const queryTimeDiff  = info.query_time_in_millis - last[ index ].query_time_in_millis;
-                        const fetchTimeDiff  = info.fetch_time_in_millis - last[ index ].fetch_time_in_millis;
+          if (last[index]) {
+            const queryTotalDiff = searchInfo.query_total - last[index].search.query_total;
+            const fetchTotalDiff = searchInfo.fetch_total - last[index].search.fetch_total;
+            const queryTimeDiff  = searchInfo.query_time_in_millis -
+                                   last[index].search.query_time_in_millis;
+            const fetchTimeDiff  = searchInfo.fetch_time_in_millis -
+                                   last[index].search.fetch_time_in_millis;
 
-                        const queryDiff = queryTimeDiff / (queryTotalDiff || 1);
-                        const fetchDiff = fetchTimeDiff / (fetchTotalDiff || 1);
+            const queryDiff = queryTimeDiff / (queryTotalDiff || 1);
+            const fetchDiff = fetchTimeDiff / (fetchTotalDiff || 1);
 
-                        r({ service: `elasticsearch.search.diff.query.${index}`, metric: queryDiff });
-                        r({ service: `elasticsearch.search.diff.fetch.${index}`, metric: fetchDiff });
-                        r({
-                            service: `elasticsearch.search.diff.query_total.${index}`,
-                            metric: queryTotalDiff
-                        });
-                        r({
-                            service: `elasticsearch.search.diff.fetch_total.${index}`,
-                            metric: fetchTotalDiff
-                        });
-                        r({
-                            service: `elasticsearch.search.diff.query_time.${index}`,
-                            metric: queryTimeDiff
-                        });
-                        r({
-                            service: `elasticsearch.search.diff.fetch_time.${index}`,
-                            metric: fetchTimeDiff
-                        });
-                    }
+            r({service: `elasticsearch.search.diff.query.${index}`, metric: queryDiff});
+            r({service: `elasticsearch.search.diff.fetch.${index}`, metric: fetchDiff});
+            r({
+              service: `elasticsearch.search.diff.query_total.${index}`,
+              metric: queryTotalDiff
+            });
+            r({
+              service: `elasticsearch.search.diff.fetch_total.${index}`,
+              metric: fetchTotalDiff
+            });
+            r({
+              service: `elasticsearch.search.diff.query_time.${index}`,
+              metric: queryTimeDiff
+            });
+            r({
+              service: `elasticsearch.search.diff.fetch_time.${index}`,
+              metric: fetchTimeDiff
+            });
 
-                    last[ index ] = info;
+            const mergeTotalDiff = mergeInfo.total - last[index].merges.total;
+            const mergeDocsDiff  = mergeInfo.total_docs - last[index].merges.total_docs;
+            const mergeSizeDiff  = mergeInfo.total_size_in_bytes -
+                                   last[index].merges.total_size_in_bytes;
+            const mergeTimeDiff  = mergeInfo.total_time_in_millis -
+                                   last[index].merges.total_time_in_millis;
 
-                    r({ service: `elasticsearch.search.query.${index}`, metric: info.query });
-                    r({ service: `elasticsearch.search.fetch.${index}`, metric: info.fetch });
+            r({
+              service: `elasticsearch.merges.diff.total.${index}`,
+              metric: mergeTotalDiff
+            });
+            r({
+              service: `elasticsearch.merges.diff.docs.${index}`,
+              metric: mergeDocsDiff
+            });
+            r({
+              service: `elasticsearch.merges.diff.size_in_bytes.${index}`,
+              metric: mergeSizeDiff
+            });
+            r({
+              service: `elasticsearch.merges.diff.time_in_millis.${index}`,
+              metric: mergeTimeDiff
+            });
+          }
 
-                    cb();
-                });
-            }, cb)
-        ], done);
-    });
+          last[index] = {search: searchInfo, merges: mergeInfo};
+
+          r({service: `elasticsearch.search.query.${index}`, metric: searchInfo.query});
+          r({service: `elasticsearch.search.fetch.${index}`, metric: searchInfo.fetch});
+
+          cb();
+        });
+      }, cb)
+    ], done);
+  });
 };
 module.exports.reset   = () => {
-    last = {};
+  last = {};
 };
 module.exports.options = {
-    'es-host': { default: 'localhost' },
-    'es-port': { default: '9200' },
-    'es-search-index': { default: [ '_all' ], multi: true }
+  'es-host': {default: 'localhost'},
+  'es-port': {default: '9200'},
+  'es-search-index': {default: ['_all'], multi: true}
 };
--- src/elasticsearch.spec.js
+++ src/elasticsearch.spec.js
@@ -5,177 +5,244 @@ const _    = require('lodash');
 const es   = require('./elasticsearch');
 
 describe('elasticsearch monitoring', () => {
-    let report, options;
-    const noop = () => {
-    };
+  let report, options;
+  const noop = () => {
+  };
 
-    const run = (done) => {
-        return (cb) => {
-            cb(options, report, done)
-        };
+  const run = (done) => {
+    return (cb) => {
+      cb(options, report, done)
+    };
+  };
+
+  beforeEach(() => {
+    options = {
+      'es-host': 'localhost',
+      'es-port': 9200,
+    };
+    report  = jasmine.createSpy('report');
+  });
+
+  describe('cluster health', () => {
+    it('query cluster health', (done) => {
+      const health = nock(/.*/).get('/_cluster/health').reply(200, {});
+
+      es({
+        run: run(() => {
+          expect(health.isDone()).toBe(true);
+          done();
+        })
+      });
+    });
+
+    it('report cluster health', (done) => {
+      nock(/.*/).get('/_cluster/health').reply(200, {one: 1, two: 2});
+
+      es({
+        run: run(() => {
+          expect(report).toHaveBeenCalledWith({
+            service: 'elasticsearch.cluster.one',
+            metric: 1
+          });
+          expect(report).toHaveBeenCalledWith({
+            service: 'elasticsearch.cluster.two',
+            metric: 2
+          });
+          done();
+        })
+      });
+    });
+  });
+
+  describe('index stats', () => {
+    let one, two;
+    const oneSearchData = {
+      query_time_in_millis: 4,
+      query_total: 4,
+      fetch_time_in_millis: 2,
+      fetch_total: 2
+    };
+    const twoSearchData = {
+      query_time_in_millis: 8,
+      query_total: 4,
+      fetch_time_in_millis: 4,
+      fetch_total: 2
+    };
+    const oneMergesData = {
+      total: 2,
+      total_docs: 2,
+      total_size_in_bytes: 4,
+      total_time_in_millis: 4,
+    };
+    const twoMergesData = {
+      total: 4,
+      total_docs: 4,
+      total_size_in_bytes: 8,
+      total_time_in_millis: 8,
+    };
+    const stats         = (index, search, merges) => {
+      const total     = {
+        search: search || {},
+        merges: merges || {}
+      };
+      const primaries = {
+        docs: {count: 26, deleted: 11},
+        store: {size_in_bytes: 26, throttle_time_in_millis: 11}
+      };
+      return nock(/.*/).get(`/${index}/_stats`)
+        .reply(200, {_all: {total: total, primaries: primaries}});
     };
 
     beforeEach(() => {
-        options = {
-            'es-host': 'localhost',
-            'es-port': 9200,
-        };
-        report  = jasmine.createSpy('report');
+      options['es-search-index'] = ['one', 'two'];
+
+      one = stats('one', oneSearchData, oneMergesData);
+      two = stats('two', twoSearchData, twoMergesData);
+
+      nock(/.*/).persist().get('/_cluster/health').reply(200, {});
     });
 
-    describe('cluster health', () => {
-        it('query cluster health', (done) => {
-            const health = nock(/.*/).get('/_cluster/health').reply(200, {});
-
-            es({
-                run: run(() => {
-                    expect(health.isDone()).toBe(true);
-                    done();
-                })
-            });
-        });
-
-        it('report cluster health', (done) => {
-            nock(/.*/).get('/_cluster/health').reply(200, { one: 1, two: 2 });
-
-            es({
-                run: run(() => {
-                    expect(report).toHaveBeenCalledWith({
-                        service: 'elasticsearch.cluster.one',
-                        metric: 1
-                    });
-                    expect(report).toHaveBeenCalledWith({
-                        service: 'elasticsearch.cluster.two',
-                        metric: 2
-                    });
-                    done();
-                })
-            });
-        });
+    afterEach(() => {
+      es.reset();
+      nock.cleanAll();
     });
 
-    describe('index stats', () => {
-        let one, two;
-        const oneData = {
-            query_time_in_millis: 4,
-            query_total: 4,
-            fetch_time_in_millis: 2,
-            fetch_total: 2
-        };
-        const twoData = {
-            query_time_in_millis: 8,
-            query_total: 4,
-            fetch_time_in_millis: 4,
-            fetch_total: 2
-        };
-        const stats   = (index, data) => {
-            const total = { search: data || {} };
-            const primaries = {
-                docs: { count: 26, deleted: 11 },
-                store: { size_in_bytes: 26, throttle_time_in_millis: 11 }
-            };
-            return nock(/.*/).get(`/${index}/_stats`).reply(200, { _all: { total: total, primaries: primaries } });
-        };
-
-        beforeEach(() => {
-            options[ 'es-search-index' ] = [ 'one', 'two' ];
-
-            one = stats('one', oneData);
-            two = stats('two', twoData);
-
-            nock(/.*/).persist().get('/_cluster/health').reply(200, {});
-        });
-
-        afterEach(() => {
-            es.reset();
-            nock.cleanAll();
-        });
-
-        it('query per given index', (done) => {
-            es({
-                run: run(() => {
-                    expect(one.isDone()).toBe(true);
-                    expect(two.isDone()).toBe(true);
-                    done();
-                })
-            });
-        });
-
-        it('write per given index', (done) => {
-            es({
-                run: run(() => {
-                    expect(report).toHaveBeenCalledWith({ service: 'elasticsearch.search.query.one', metric: 1 });
-                    expect(report).toHaveBeenCalledWith({ service: 'elasticsearch.search.fetch.one', metric: 1 });
-                    expect(report).toHaveBeenCalledWith({ service: 'elasticsearch.search.query.two', metric: 2 });
-                    expect(report).toHaveBeenCalledWith({ service: 'elasticsearch.search.fetch.two', metric: 2 });
-                    expect(report).toHaveBeenCalledWith({ service: 'elasticsearch.search.fetch.two', metric: 2 });
-                    expect(report).toHaveBeenCalledWith({ service: 'elasticsearch.docs.one.count', metric: 26 });
-                    expect(report).toHaveBeenCalledWith({ service: 'elasticsearch.docs.one.deleted', metric: 11 });
-                    expect(report).toHaveBeenCalledWith({ service: 'elasticsearch.docs.two.count', metric: 26 });
-                    expect(report).toHaveBeenCalledWith({ service: 'elasticsearch.docs.two.deleted', metric: 11 });
-                    expect(report).toHaveBeenCalledWith({
-                        service: 'elasticsearch.store.one.size_in_bytes',
-                        metric: 26
-                    });
-                    expect(report).toHaveBeenCalledWith({
-                        service: 'elasticsearch.store.one.throttle_time_in_millis',
-                        metric: 11
-                    });
-                    expect(report).toHaveBeenCalledWith({
-                        service: 'elasticsearch.store.two.size_in_bytes',
-                        metric: 26
-                    });
-                    expect(report).toHaveBeenCalledWith({
-                        service: 'elasticsearch.store.two.throttle_time_in_millis',
-                        metric: 11
-                    });
-
-                    done();
-                })
-            });
-        });
-
-        it('write diff per index on second call', (done) => {
-            es({
-                run: run(() => {
-                    one = stats('one', {
-                        query_time_in_millis: 12,
-                        query_total: 8,
-                        fetch_time_in_millis: 6,
-                        fetch_total: 4
-                    });
-                    two = stats('two', {
-                        query_time_in_millis: 12,
-                        query_total: 8,
-                        fetch_time_in_millis: 6,
-                        fetch_total: 4
-                    });
-
-                    es({
-                        run: run(() => {
-                            expect(report).toHaveBeenCalledWith({
-                                service: 'elasticsearch.search.diff.query.one',
-                                metric: 2
-                            });
-                            expect(report).toHaveBeenCalledWith({
-                                service: 'elasticsearch.search.diff.fetch.one',
-                                metric: 2
-                            });
-                            expect(report).toHaveBeenCalledWith({
-                                service: 'elasticsearch.search.diff.query.two',
-                                metric: 1
-                            });
-                            expect(report).toHaveBeenCalledWith({
-                                service: 'elasticsearch.search.diff.fetch.two',
-                                metric: 1
-                            });
-
-                            done();
-                        })
-                    });
-                })
-            });
-        });
+    it('query per given index', (done) => {
+      es({
+        run: run(() => {
+          expect(one.isDone()).toBe(true);
+          expect(two.isDone()).toBe(true);
+          done();
+        })
+      });
     });
+
+    it('write per given index', (done) => {
+      es({
+        run: run(() => {
+          expect(report)
+            .toHaveBeenCalledWith({service: 'elasticsearch.search.query.one', metric: 1});
+          expect(report)
+            .toHaveBeenCalledWith({service: 'elasticsearch.search.fetch.one', metric: 1});
+          expect(report)
+            .toHaveBeenCalledWith({service: 'elasticsearch.search.query.two', metric: 2});
+          expect(report)
+            .toHaveBeenCalledWith({service: 'elasticsearch.search.fetch.two', metric: 2});
+          expect(report)
+            .toHaveBeenCalledWith({service: 'elasticsearch.docs.one.count', metric: 26});
+          expect(report)
+            .toHaveBeenCalledWith({service: 'elasticsearch.docs.one.deleted', metric: 11});
+          expect(report)
+            .toHaveBeenCalledWith({service: 'elasticsearch.docs.two.count', metric: 26});
+          expect(report)
+            .toHaveBeenCalledWith({service: 'elasticsearch.docs.two.deleted', metric: 11});
+          expect(report).toHaveBeenCalledWith({
+            service: 'elasticsearch.store.one.size_in_bytes',
+            metric: 26
+          });
+          expect(report).toHaveBeenCalledWith({
+            service: 'elasticsearch.store.one.throttle_time_in_millis',
+            metric: 11
+          });
+          expect(report).toHaveBeenCalledWith({
+            service: 'elasticsearch.store.two.size_in_bytes',
+            metric: 26
+          });
+          expect(report).toHaveBeenCalledWith({
+            service: 'elasticsearch.store.two.throttle_time_in_millis',
+            metric: 11
+          });
+
+          done();
+        })
+      });
+    });
+
+    it('write diff per index on second call', (done) => {
+      es({
+        run: run(() => {
+          one = stats('one', {
+            query_time_in_millis: 12,
+            query_total: 8,
+            fetch_time_in_millis: 6,
+            fetch_total: 4
+          }, {
+            total: 4,
+            total_docs: 4,
+            total_size_in_bytes: 6,
+            total_time_in_millis: 6,
+          });
+          two = stats('two', {
+            query_time_in_millis: 12,
+            query_total: 8,
+            fetch_time_in_millis: 6,
+            fetch_total: 4
+          }, {
+            total: 5,
+            total_docs: 5,
+            total_size_in_bytes: 9,
+            total_time_in_millis: 9,
+          });
+
+          es({
+            run: run(() => {
+              expect(report).toHaveBeenCalledWith({
+                service: 'elasticsearch.search.diff.query.one',
+                metric: 2
+              });
+              expect(report).toHaveBeenCalledWith({
+                service: 'elasticsearch.search.diff.fetch.one',
+                metric: 2
+              });
+              expect(report).toHaveBeenCalledWith({
+                service: 'elasticsearch.search.diff.query.two',
+                metric: 1
+              });
+              expect(report).toHaveBeenCalledWith({
+                service: 'elasticsearch.search.diff.fetch.two',
+                metric: 1
+              });
+
+              expect(report).toHaveBeenCalledWith({
+                service: 'elasticsearch.merges.diff.total.one',
+                metric: 2
+              });
+              expect(report).toHaveBeenCalledWith({
+                service: 'elasticsearch.merges.diff.docs.one',
+                metric: 2
+              });
+              expect(report).toHaveBeenCalledWith({
+                service: 'elasticsearch.merges.diff.size_in_bytes.one',
+                metric: 2
+              });
+              expect(report).toHaveBeenCalledWith({
+                service: 'elasticsearch.merges.diff.time_in_millis.one',
+                metric: 2
+              });
+
+              expect(report).toHaveBeenCalledWith({
+                service: 'elasticsearch.merges.diff.total.two',
+                metric: 1
+              });
+              expect(report).toHaveBeenCalledWith({
+                service: 'elasticsearch.merges.diff.docs.two',
+                metric: 1
+              });
+              expect(report).toHaveBeenCalledWith({
+                service: 'elasticsearch.merges.diff.size_in_bytes.two',
+                metric: 1
+              });
+              expect(report).toHaveBeenCalledWith({
+                service: 'elasticsearch.merges.diff.time_in_millis.two',
+                metric: 1
+              });
+
+              done();
+            })
+          });
+        })
+      });
+    });
+  });
 });
 
